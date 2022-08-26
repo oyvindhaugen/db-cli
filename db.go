17479a083e2907f35ef413a455365a12b70bcfd3
oyvindhaugen <oyvindet@gmail.com>
2022-08-26 14:21:13 +0200

    Ready for further development.
    -Make UI (HTML File) for each action.
    -Make select display when called
    -CSS

---
+++ db.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+var ActionInt int
+var Id int
+var Item string
+var Amount int
+
+type columns struct {
+	id     int
+	item   string
+	amount int
+}
+
+//this does all the database stuff
+func decide() {
+	fmt.Println("here")
+	fmt.Println(ActionInt)
+	if ActionInt == 1 {
+		insert(&columns{Id, Item, Amount})
+	} else if ActionInt == 2 {
+		del(&columns{Id, Item, Amount})
+	} else if ActionInt == 3 {
+		updt(&columns{Id, Item, Amount})
+	} else if ActionInt == 4 {
+		slct(&columns{Id, Item, Amount})
+	} else {
+		fmt.Println("Error")
+	}
+
+}
+func CheckError(err error) {
+	if err != nil {
+		log.Fatal()
+	}
+}
+
+var pass string = ""
+
+func insert(c *columns) error {
+	psqlconn := fmt.Sprintf("host= localhost port = 5432 user = postgres password = %s  dbname = postgres sslmode=disable", pass)
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+	defer db.Close()
+
+	_, err = db.Exec("insert into shopping (item, amount) values ($1, $2)", c.item, c.amount)
+	if err != nil {
+		fmt.Println("mo")
+		return err
+	}
+	fmt.Println("Success")
+	return nil
+}
+func del(c *columns) error {
+	psqlconn := fmt.Sprintf("host= localhost port = 5432 user = postgres password = %s  dbname = first_db sslmode=disable", pass)
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+	defer db.Close()
+
+	_, err = db.Exec("delete from shopping where id = $1;", c.id)
+	if err != nil {
+		fmt.Println("mo")
+		return err
+	}
+	fmt.Println("Success")
+	return nil
+}
+func updt(c *columns) error {
+
+	psqlconn := fmt.Sprintf("host= localhost port = 5432 user = postgres password = %s  dbname = first_db sslmode=disable", pass)
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+	defer db.Close()
+
+	_, err = db.Query("update shopping set item = $2, amount = $3 where id = $1", c.id, c.item, c.amount)
+	if err != nil {
+		fmt.Println("mo")
+		return err
+	}
+	fmt.Println("Success")
+	return nil
+}
+func slct(c *columns) error {
+
+	psqlconn := fmt.Sprintf("host= localhost port = 5432 user = postgres password = %s  dbname = first_db sslmode=disable", pass)
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+	defer db.Close()
+
+	var (
+		id     int
+		item   string
+		amount int
+	)
+	res, err := db.Query("select * from shopping where id = $1;", c.id)
+	if err != nil {
+		fmt.Println("mo")
+		return err
+	}
+	defer res.Close()
+	for res.Next() {
+		err := res.Scan(&id, &item, &amount)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("id: %d\n item: %s\n amount: %d\n", id, item, amount)
+	}
+
+	fmt.Println("Success")
+	return nil
+}
--- website.go
+++ website.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 )
 
-func queryHandler(w http.ResponseWriter, r *http.Request) {
+// this is the handler for cli.html and it gives all the parameters needed for database queries
+func cliHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful\n")
+	id := r.FormValue("id")
+	Id, _ = strconv.Atoi(id)
+	Item = r.FormValue("item")
+	amount := r.FormValue("amount")
+	Amount, _ = strconv.Atoi(amount)
+	go decide()
+}
+
+// this tells the db.go what database action is gonna be performed
+func formTestHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
@@ -19,5 +36,11 @@ func queryHandler(w http.ResponseWriter, r *http.Request) {
 func handle() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-	http.HandleFunc("/index", queryHandler)
+	http.HandleFunc("/index", formTestHandler)
+	http.HandleFunc("/cli", cliHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
